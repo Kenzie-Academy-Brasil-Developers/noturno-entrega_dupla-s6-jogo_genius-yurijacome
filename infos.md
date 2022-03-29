388f4a05bd034e475323e50dbcdfc21272ce5df3
Yuri Jácome <yurijacome@hotmail.com>
2022-03-29 13:09:02 -0300

    second push

---
+++ infos.md
@@ -0,0 +1,2 @@
+apertar start deve sortiar uma cor
+SE acertar a  cor continuar a sequencia
---
+++ scrypt.js
@@ -0,0 +1,9 @@
+ 
+ const redButton = document.getElementById("red")
+ const blueButton = document.getElementById("blue")
+ const yellowButton = document.getElementById("yellow")
+ const greenButton = document.getElementById("green")
+
+ const startButton = document.getElementById("startButton")
+
+ 
